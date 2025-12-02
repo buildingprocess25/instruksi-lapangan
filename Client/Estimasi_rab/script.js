b821b2d6851b63630448615de0d8c3e2cb4a7025
Dimas Abidzar Fadly <dimasfadly01@gmail.com>
2025-12-02 17:23:34 +0700

    update base url (oracle)

--- Client/Estimasi_rab/script.js
+++ Client/Estimasi_rab/script.js
@@ -15,8 +15,8 @@ let rejectedSubmissionsList = [];
 let originalFormData = null;
 
 
-const PYTHON_API_BASE_URL = "https://instruksi-lapangan.onrender.com";
-// const PYTHON_API_BASE_URL = "https://cuma-backend.web.id";
+// const PYTHON_API_BASE_URL = "https://instruksi-lapangan.onrender.com";
+const PYTHON_API_BASE_URL = "https://cuma-backend.web.id";
 
 const sipilCategoryOrder = [
     "PEKERJAAN PERSIAPAN",
